b702dde467c916d565a6a8c572fd4e064d6714e4
onesome <onesomegit@outlook.com>
2023-06-13 00:45:37 -0500

    QNA: Polish
    Still non functonal

--- static/qna.js
+++ static/qna.js
@@ -9,16 +9,61 @@ const qnaOpenButton = $el("#help-button");
 const qnaCloseButton = $el("#close-qna-button");
 const qnaChat = $el("#qna-chat");
 const chatMessageContainer = $el("#chat-messages");
+const chatInput = $el("#chat-input");
+const typingIndicator = $el("#typing-indicator");
 
 qnaOpenButton.addEventListener("click", function () {
     qnaOpenButton.classList.add("hidden");
     qnaChat.classList.remove("hidden");
+    chatInput.focus();
 });
 
 qnaCloseButton.addEventListener("click", function () {
     qnaOpenButton.classList.remove("hidden");
     qnaChat.classList.add("hidden");
 });
+
+chatInput.addEventListener("keydown", function (event) {
+    if (event.key !== "Enter") return;
+
+    const userPrompt = chatInput.value;
+
+    renderMessage(Author.USER, userPrompt);
+    chatInput.value = "";
+
+    // Prevent desync and other shenanigans
+    chatInput.disabled = true;
+
+    // If we make it look like something is happening it will seem like something is happening
+    setTimeout(function () {
+        setTyping(true);
+    }, 150);
+
+    setTimeout(function () {
+        const botMessage = getBotResponse(chatInput.value);
+        renderMessage(Author.BOT, botMessage);
+        setTyping(false);
+
+        chatInput.disabled = false;
+        chatInput.focus();
+    }, 1700);
+});
+
+function getBotResponse(message) {
+    return "WHAT"
+}
+
+function setTyping(isTyping) {
+    if (!isTyping) {
+        const typingEl = $el("#typing-indicator");
+        if (typingEl) typingEl.remove();
+        return;
+    }
+
+    const messageWrapper = $e("div", chatMessageContainer, { classes: ["chat-message-wrapper"], id: "typing-indicator" });
+    $e("div", messageWrapper, { classes: ["chat-message", "bot"], innerText: `${BOT_NAME} is thinking...` });
+    messageWrapper.scrollIntoView();
+}
 
 function renderMessage(author, content) {
     // Shows a message in the chat given an author of enum type Author and content string
@@ -38,6 +83,7 @@ function renderMessage(author, content) {
     if (!alignLeft) $e("spacer", messageWrapper);
     $e("div", messageWrapper, { classes: ["chat-message", authorCSSClass], innerText: content });
     if (alignLeft) $e("spacer", messageWrapper);
+    messageWrapper.scrollIntoView();
 }
 
 renderMessage(Author.BOT, "Hello, and welcome to Spirit!");
