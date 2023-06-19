4c99546b449b97ef4205ade2df63107d54d7f85a
Naught0 <wasiuddin2007@gmail.com>
2023-06-19 15:53:31 -0500

    k

---
+++ audit_log.py
@@ -0,0 +1,60 @@
+import json
+import os
+import random
+import sqlite3
+from typing import Optional
+
+import querymaker
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyz-0123456789"
+ROLLBACK_ID_LENGTH = 8
+ROLLBACK_DIR = "backup/restore_points"
+
+
+def get_rollback_id() -> str:
+    """Gets a unique 8 digit identifier string for storing rollback db copies.
+
+    Returns:
+        str: The rollback ID generated
+    """
+    rollback_id = None
+    while (not rollback_id) or os.path.exists(
+        os.path.join(ROLLBACK_DIR, rollback_id + ".db")
+    ):
+        rollback_id = "".join(random.sample(ALPHABET, ROLLBACK_ID_LENGTH))
+    return rollback_id
+
+
+def report_event(
+    user_name: str,
+    action: str,
+    details: Optional[dict] = None,
+    allow_rollback: bool = False,
+) -> None:
+    """Reports events to the audit log database, where they can be viewed by
+    the user in the web app at a later time. Must be called BEFORE any database
+    action is made.
+
+    Args:
+        user_name (str): The name of the user performing the action
+        action (str): The action being performed
+        details (Optional[dict], optional): Dict of action details. Defaults to None.
+        allow_rollback (bool, optional): If a backup is made to allow for rollbacks later. Defaults to False.
+    """
+    details = details or {}
+    con = querymaker.con()
+
+    rollback_id = None
+    if allow_rollback:
+        rollback_id = get_rollback_id()
+        with sqlite3.connect(
+            os.path.join(ROLLBACK_DIR, rollback_id + ".db")
+        ) as rollback_db:
+            con.backup(rollback_db)
+
+    con.execute(
+        "INSERT INTO audit_log(time, user, action, details, rollback_id) VALUES(UNIXEPOCH(), ?, ?, ?, ?)",
+        (user_name, action, json.dumps(details) if details else None, rollback_id),
+    )
+    con.commit()
+    con.close()
---
+++ static/audit-log.js
@@ -0,0 +1,86 @@
+const LOG_SELECTED_CLASS = "log-selected";
+
+const auditLogEl = $el("#audit-log");
+const auditDetailTextEl = $el("#audit-detail-text");
+let initDetailShown = false;
+
+async function tryRevertAuditEvent(eventId) {
+    const doIt = await modalConfirm("Really revert the database to this point in time? This action is not reversable.", "Revert", "Cancel");
+    if (!doIt) return;
+    console.log("TODO: REVERT TO", eventId);
+}
+
+function showAuditEventDetails(entry) {
+    const date = new Date(entry.time * 1000);
+    let actionDeets = [];
+
+    for (const [key, value] of Object.entries(entry.details)) {
+        actionDeets.push(`- ${key}: ${value}`);
+    };
+
+    actionDeets = actionDeets.join("\n");
+
+    auditDetailTextEl.innerText = `Time: ${date.toLocaleString()}
+    User Responsible: ${entry.user}
+    Action Type: ${entry.action}
+    Can Revert: ${entry.has_checkpoint ? "Yes" : "No"}`;
+
+    if (actionDeets) auditDetailTextEl.innerText += `\nAction Details: \n${actionDeets}`;
+
+}
+
+function highlightEntry(el) {
+    for (const highlighted of document.getElementsByClassName(LOG_SELECTED_CLASS)) {
+        highlighted.classList.remove(LOG_SELECTED_CLASS);
+    }
+
+    el.classList.add(LOG_SELECTED_CLASS);
+}
+
+function renderAuditLogEntry(entry) {
+    const entryEl = $e("div", auditLogEl, { classes: ["audit-log-entry"] });
+    const date = new Date(entry.time * 1000);
+    const timeEl = $e("span", entryEl, { innerText: date.toLocaleString(), classes: ["al-time"] });
+    const userEl = $e("span", entryEl, { innerText: entry.user, classes: ["al-user"] });
+    const actionEl = $e("span", entryEl, { innerText: entry.action, classes: ["al-action"] });
+
+    if (entry.has_checkpoint) {
+        $e("spacer", entryEl);
+        const revertEl = $e(
+            "span",
+            entryEl,
+            {
+                innerText: "update",
+                title: "Revert to this point",
+                classes: ["al-revert", "material-icons"]
+            }
+        );
+
+        revertEl.addEventListener("click", async function (event) {
+            event.stopPropagation();
+            await tryRevertAuditEvent(entry.event_id);
+        });
+    }
+
+    entryEl.addEventListener("click", function () {
+        highlightEntry(entryEl);
+        showAuditEventDetails(entry);
+    });
+
+    if (!initDetailShown) {
+        initDetailShown = true;
+        highlightEntry(entryEl);
+        showAuditEventDetails(entry);
+    }
+}
+
+async function loadAuditLog() {
+    const r = await fetch("/api/audit_log.json");
+    const j = await r.json();
+
+    for (const entry of j) {
+        renderAuditLogEntry(entry);
+    }
+}
+
+loadAuditLog();
